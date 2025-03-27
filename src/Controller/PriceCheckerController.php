b027d16d111438cb448eae69b44c480f8759204d
PawelMTRK <pawelmat@onet.pl>
2025-03-27 17:32:29 +0100

    - make Item url type bigger - add basic item display

--- src/Controller/PriceCheckerController.php
+++ src/Controller/PriceCheckerController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Item;
 
 final class PriceCheckerController extends AbstractController
 {
     #[Route('/', name: 'app_price_checker')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManagerInterface): Response
     {
+        $items = $entityManagerInterface->getRepository(Item::class)->findAll();
         return $this->render('price_checker/index.html.twig', [
             'controller_name' => 'PriceCheckerController',
+            'items' => $items
         ]);
     }
 }
--- src/Entity/Item.php
+++ src/Entity/Item.php
@@ -18,7 +18,7 @@ class Item
     #[ORM\Column(length: 255)]
     private ?string $name = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 500)]
     private ?string $url = null;
 
     #[ORM\ManyToOne(inversedBy: 'items')]
